4c98e0bcbc1e6d58cca49fedb41ce4d858037b71
Hpt9 <Hpt9@users.noreply.github.com>
2024-08-01 00:59:15 +0000

    deploy: ec869e2730f70bfaee061b6057514563efa196e1

--- src/components/MobileMenu.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-export default function MobileMenu() {
-  return (
-    <div>MobileMenu</div>
-  )
-}
--- src/components/Setting.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Setting() {
-  return (
-    <div>Setting</div>
-  )
-}
